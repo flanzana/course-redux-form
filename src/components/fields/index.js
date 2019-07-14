89a59e031a1c7939c0e879e05dae63649ccb2b0a
Zana Flander <zana.flander@kiwi.com>
2019-07-14 15:26:38 +0200

    Feat: add react json view dependency

--- src/components/fields/index.js
+++ src/components/fields/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import ReactJson from 'react-json-view'; // displays all props of the component
 
 export const customInput = props => {
   return (
     <div>
       <label>{props.label}</label>
       <input type={props.type} {...props.input} />
+      <ReactJson src={props.meta} />
     </div>
   )
 };
@@ -17,6 +19,7 @@ export const customSelect = props => {
         <option value="tabs">Tabs</option>
         <option value="spaces">Spaces</option>
       </select>
+      <ReactJson src={props.meta} />
     </div>
   )
 };
